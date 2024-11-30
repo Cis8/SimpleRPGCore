7b0525ba06956599c8905fd3471bd9a6a895186a
Cis8 <ema.cis8@gmail.com>
2024-11-30 16:05:02 +0100

    Add custom editor for InRef, LongRef

---
+++ Editor/ElectricDrill/Utils/IntRefPropertyDrawer.cs
@@ -0,0 +1,43 @@
+using UnityEditor;
+using UnityEngine;
+using ElectricDrill.SimpleRpgCore.Utils;
+
+namespace ElectricDrill.SimpleRpgCore
+{
+    [CustomPropertyDrawer(typeof(IntRef))]
+    public class IntRefPropertyDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            EditorGUI.BeginProperty(position, label, property);
+
+            var useConstant = property.FindPropertyRelative("UseConstant");
+            var constantValue = property.FindPropertyRelative("ConstantValue");
+            var variable = property.FindPropertyRelative("Variable");
+
+            position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
+
+            var indent = EditorGUI.indentLevel;
+            EditorGUI.indentLevel = 0;
+
+            var toggleRect = new Rect(position.x, position.y, 20, position.height);
+            var labelRect = new Rect(position.x + 25, position.y, 100, position.height);
+            var valueRect = new Rect(position.x + 130, position.y, position.width - 130, position.height);
+
+            useConstant.boolValue = EditorGUI.Toggle(toggleRect, useConstant.boolValue);
+            EditorGUI.LabelField(labelRect, "Use Constant");
+
+            if (useConstant.boolValue)
+            {
+                constantValue.intValue = EditorGUI.IntField(valueRect, constantValue.intValue);
+            }
+            else
+            {
+                EditorGUI.PropertyField(valueRect, variable, GUIContent.none);
+            }
+
+            EditorGUI.indentLevel = indent;
+            EditorGUI.EndProperty();
+        }
+    }
+}
---
+++ Editor/ElectricDrill/Utils/LongRefPropertyDrawer.cs
@@ -0,0 +1,43 @@
+using UnityEditor;
+using UnityEngine;
+using ElectricDrill.SimpleRpgCore.Utils;
+
+namespace ElectricDrill.SimpleRpgCore
+{
+    [CustomPropertyDrawer(typeof(LongRef))]
+    public class LongRefPropertyDrawer : PropertyDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            EditorGUI.BeginProperty(position, label, property);
+
+            var useConstant = property.FindPropertyRelative("UseConstant");
+            var constantValue = property.FindPropertyRelative("ConstantValue");
+            var variable = property.FindPropertyRelative("Variable");
+
+            position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
+
+            var indent = EditorGUI.indentLevel;
+            EditorGUI.indentLevel = 0;
+
+            var toggleRect = new Rect(position.x, position.y, 20, position.height);
+            var labelRect = new Rect(position.x + 25, position.y, 100, position.height);
+            var valueRect = new Rect(position.x + 130, position.y, position.width - 130, position.height);
+
+            useConstant.boolValue = EditorGUI.Toggle(toggleRect, useConstant.boolValue);
+            EditorGUI.LabelField(labelRect, "Use Constant");
+
+            if (useConstant.boolValue)
+            {
+                constantValue.longValue = EditorGUI.LongField(valueRect, constantValue.longValue);
+            }
+            else
+            {
+                EditorGUI.PropertyField(valueRect, variable, GUIContent.none);
+            }
+
+            EditorGUI.indentLevel = indent;
+            EditorGUI.EndProperty();
+        }
+    }
+}
