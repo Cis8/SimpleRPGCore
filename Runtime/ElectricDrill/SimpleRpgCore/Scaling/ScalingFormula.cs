50c9c9fc52a9457e7dd1e333fa91ada931b9a9d4
Emanuele Cisotto <emanuele.cisotto@kynetics.com>
2024-12-02 17:19:00 +0100

    Add scaling base value for ScalingFormula

--- Runtime/ElectricDrill/SimpleRpgCore/Scaling/ScalingFormula.cs
+++ Runtime/ElectricDrill/SimpleRpgCore/Scaling/ScalingFormula.cs
@@ -10,20 +10,42 @@ namespace ElectricDrill.SimpleRpgCore.Scaling {
     [CreateAssetMenu(fileName = "New Scaling Formula", menuName = "Simple RPG/Scaling/Scaling Formula")]
     public class ScalingFormula : ScriptableObject
     {
-        [SerializeField] private long _baseValue;
-        [SerializeField] private List<ScalingComponent> _selfScalingComponents;
-        [SerializeField] private List<ScalingComponent> _targetScalingComponents;
+        [SerializeField] private bool useScalingBaseValue;
+        [SerializeField] private GrowthFormula scalingBaseValue;
+        [SerializeField] private long fixedBaseValue;
+        [SerializeField] private List<ScalingComponent> selfScalingComponents;
+        [SerializeField] private List<ScalingComponent> targetScalingComponents;
         
         public long CalculateValue(EntityCore entity) {
-            Assert.IsTrue(_targetScalingComponents.Count == 0, "This formula requires a target entity to calculate the value");
-            return _baseValue +
-                   _selfScalingComponents.Sum(component => component.CalculateValue(entity));
+            Assert.IsTrue(targetScalingComponents.Count == 0, "This formula requires a target entity to calculate the value");
+            return CalculateBaseValue() +
+                   selfScalingComponents.Sum(component => component.CalculateValue(entity));
+        }
+        
+        public long CalculateValue(EntityCore self, int level) {
+            Assert.IsTrue(targetScalingComponents.Count == 0, "This formula requires a target entity to calculate the value");
+            return CalculateBaseValue(level) +
+                   selfScalingComponents.Sum(component => component.CalculateValue(self));
         }
         
         public long CalculateValue(EntityCore self, EntityCore target) {
-            return _baseValue +
-                   _selfScalingComponents.Sum(component => component.CalculateValue(self)) +
-                   _targetScalingComponents.Sum(component => component.CalculateValue(target));
+            return CalculateBaseValue() +
+                   selfScalingComponents.Sum(component => component.CalculateValue(self)) +
+                   targetScalingComponents.Sum(component => component.CalculateValue(target));
+        }
+        
+        public long CalculateValue(EntityCore self, EntityCore target, int level) {
+            return CalculateBaseValue(level) +
+                   selfScalingComponents.Sum(component => component.CalculateValue(self)) +
+                   targetScalingComponents.Sum(component => component.CalculateValue(target));
+        }
+
+        private long CalculateBaseValue(int level = 0) {
+            Assert.IsTrue(!useScalingBaseValue || scalingBaseValue != null,
+                "Scaling base value growth formula shall be set when Use Scaling Base Value is set true");
+            Assert.IsTrue(!useScalingBaseValue || level > 0,
+                "Level shall be greater than 0 when Use Scaling Base Value is set true");
+            return useScalingBaseValue ? scalingBaseValue.GetGrowthValue(level) : fixedBaseValue;
         }
     }
 }
