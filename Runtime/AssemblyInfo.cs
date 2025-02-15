b33f2625813e0f5dc3d8c352487f1cdd35befc0c
Cis8 <ema.cis8@gmail.com>
2025-02-16 00:00:43 +0100

    Add PlayModeTests.cs to allowed internals visible to

--- Runtime/AssemblyInfo.cs
+++ Runtime/AssemblyInfo.cs
@@ -2,5 +2,6 @@ using System.Runtime.CompilerServices;
 [assembly: InternalsVisibleTo("ElectricDrill.SimpleRPGCore.Editor")]
 [assembly: InternalsVisibleTo("com.electricdrill.rpg-stats-core-tests.Tests")]
 [assembly: InternalsVisibleTo("com.electricdrill.rpg-stats-core-tests.PlayModeTests")]
+[assembly: InternalsVisibleTo("com.electricdrill.rpg-stats-core-tests.TestUtils")]
 [assembly: InternalsVisibleTo("com.electricdrill.rpg-stats-health-tests.Tests")]
 [assembly: InternalsVisibleTo("ElectricDrill.SimpleRPGHealth.Editor")]
